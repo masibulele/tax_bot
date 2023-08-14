38c340d2ca5a09ea5849162e6964bc2c7470a8fa
Masibulele <masibulele100@gmail.com>
2023-08-14 19:54:20 +0200

    add retreivial tool to sets of tool

--- tax_agent.py
+++ tax_agent.py
@@ -4,7 +4,10 @@ from langchain.agents import load_tools
 from langchain.agents import  initialize_agent
 from langchain.agents import AgentType
 from langchain.llms import OpenAI
-
+from langchain.chains import RetrievalQA
+from langchain.agents import Tool
+from load_data_to_database import create_Vector_db , get_source_data_pdf, create_chunks
+from langchain.tools import BaseTool
 
 
 #including envirnoment variables
@@ -14,15 +17,31 @@ load_dotenv()
 llm= OpenAI(temperature=0)
 
 #create tools
-tool_names_list =['serpapi','llm-math']
+# create retrivial tool of individul taxes vector db
+file_name= 'data\old_tax_guide.pdf'
+document= get_source_data_pdf(file=file_name)
+doc_chunks= create_chunks(document)
+db =create_Vector_db(doc_chunks)
+individul_tax= RetrievalQA.from_chain_type(llm=llm,chain_type="stuff",retriever=db.as_retriever())
+tax= Tool(
+    name='Individual Tax QA',
+    func= individul_tax.run,
+    description= "useful when answering south african income tax questions for an individual. Input should be fully formed sentence"
+
+)
+tool_names_list =['serpapi','llm-math',]
 tools = load_tools(tool_names=tool_names_list,llm=llm)
+tools.append(tax)
+
 
 # initialize agent with tools, llm, type of agent
-
 agent = initialize_agent(tools,llm=llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 
 if __name__ == "__main__":
     query="Who is Leo DiCaprio's girlfriend? What is her current age raised to the 2 power?"
+    query_2= "would Leo DiCaprio be required to pay taxes in south africa"
     response=agent.run(query)
-    print(response)
+    print(f'Question 1:{query}\n ',response)
+    response=agent.run(query_2)
+    print(f'Question 2:{query_2}\n ',response)

627b71c797dac711c77b67ea56a5497b846ae7ec
Masibulele <masibulele100@gmail.com>
2023-08-23 11:56:05 +0200

    update load database function

--- load_data_to_database.py
+++ load_data_to_database.py
@@ -3,9 +3,18 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from  dotenv import load_dotenv
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.llms import OpenAI
 
 #loading environment variables
 load_dotenv()
+
+#create chroma client
+
+collection_name= 'tax_guide_individual'
+persist_directory='db'
+
+
 
 #get data from  source documents
 
@@ -28,7 +37,8 @@ def create_chunks(data_list_object):
 def create_Vector_db(chunk_list):
     """takes in chunk list and creates a vector database"""
     embeddings_model= OpenAIEmbeddings()
-    db= Chroma.from_documents(chunk_list,embeddings_model)
+    db= Chroma.from_documents(chunk_list,embeddings_model,collection_name=collection_name,persist_directory=persist_directory)
+    db.persist()
     return db
 
 
@@ -36,24 +46,49 @@ def create_Vector_db(chunk_list):
 # create a query function
 def get_similarity_search(query,db,k=2):
     """Takes in query creates a query embding and returns response"""
-    embed_query = OpenAIEmbeddings().embed_query(query)
+    embed_query = get_embed_query(query)
     similar_doc= db.similarity_search_by_vector(embed_query,k=k)
     return similar_doc
 
+# Load database from persist database in disk
+def load_db_from_disk():
+    """Load database from persist database in disk"""
+    embedding_model= OpenAIEmbeddings()
+    vectordb= Chroma(persist_directory=persist_directory,embedding_function=embedding_model)
+    return vectordb
+
+#get collection to query from client
+def get_collection(client):
+    embeddings= OpenAIEmbeddings()
+    tax_collection= client.get_collection(collection_name,embeddings)
+    return tax_collection
+
+def get_embed_query(query):
+     embed_query=OpenAIEmbeddings().embed_query(query)
+     return embed_query
+
 
 if __name__ == '__main__':
-    file_path='data\old_tax_guide.pdf'
-    document= get_source_data_pdf(file_path)
-    # print(document)
-    # print(type(document))
+    # file_path='data\Legal-Pub-Guide-IT01-Guide-on-Income-Tax-and-the-Individual.pdf'
+    # document= get_source_data_pdf(file_path)
+    # # print(document)
+    # # print(type(document))
 
-    doc_chunks= create_chunks(document)
-    # print(len(doc_chunks))
-    # print(doc_chunks[0])
-    # print(type(doc_chunks))
+    # doc_chunks= create_chunks(document)
+    # # # print(len(doc_chunks))
+    # # # print(doc_chunks[0])
+    # # # print(type(doc_chunks))
 
-    db = create_Vector_db(doc_chunks)
+    # db = create_Vector_db(doc_chunks)
+    # db=None
+    
+
+   
+
+    db= load_db_from_disk()
+    qa2 = RetrievalQA.from_chain_type(llm=OpenAI(),chain_type="stuff",retriever=db.as_retriever())
     query = 'what is income tax'
+    response= qa2.run(query)
+    print(response)
 
-    ans= get_similarity_search(query,db)
-    print(ans)
+
